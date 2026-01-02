7d3627938fafb703f4eabe749d3deac4588d8fd8
harryvieth <60831112+harryvieth@users.noreply.github.com>
2026-01-02 09:41:34 -0500

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from config import db
 
 
 class User(db.Model):
-    __tablename__ = 'users'  # 👈 explicit table name
+    __tablename__ = 'users'  
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
